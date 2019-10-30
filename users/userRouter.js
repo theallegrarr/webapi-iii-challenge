179617467a3c7faae118bad31889c9e0063d0c5b
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-30 13:42:19 +0100

    validate user middleware done

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,8 +3,8 @@ const users = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser, (req, res) => {
+  res.json(req.user);
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -43,11 +43,23 @@ function validateUserId(req, res, next) {
          res.status(400).json({ message: 'User id is not valid' });
        }).catch(error => {
          console.log(error);
+         res.status(400).json({ message: 'Error fetching user' });
        })
 };
 
 function validateUser(req, res, next) {
-
+  if(!req.body.name)res.status(400).json({ message: 'User missing required fields' });
+    users.insert(req.body)
+        .then(user => {
+          if(user) {
+            req.user=user;
+            next();
+          }
+          res.status(400).json({ message: 'User id is not valid' });
+        }).catch(error => {
+          console.log(error);
+          res.status(400).json({ message: 'Error adding user valid' });
+        })
 };
 
 function validatePost(req, res, next) {
